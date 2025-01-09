fe4ee4649218cfb352b08a58975dc4ef095ac0dc
gtiazara <tiazaragaetan@gmail.com>
2025-01-09 17:22:02 +0100

    init project with iconic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agoraspot',
+  webDir: 'dist'
+};
+
+export default config;
