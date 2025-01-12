8afcd7efe34d07330e7ab5d2ab98caeb93296ae2
gtiazara <tiazaragaetan@gmail.com>
2025-01-12 17:12:54 +0100

    new test remoge jage call

--- server/app.js
+++ server/app.js
@@ -20,8 +20,8 @@ const bodyParser = require("body-parser");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
