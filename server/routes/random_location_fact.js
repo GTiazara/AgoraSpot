cf8909738833642249b1f1c892dc2eb6ae82cfc2
gtiazara <tiazaragaetan@gmail.com>
2025-02-16 05:51:09 +0100

    suitwh fact to 5 min

--- server/routes/random_location_fact.js
+++ server/routes/random_location_fact.js
@@ -31,8 +31,8 @@ router.get("/", async (req, res) => {
 
         // Get current timestamp
         const now = new Date();
-        const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000); // 1 hour ago
-
+        const oneHourAgo = new Date(now.getTime() - 5 * 60 * 1000); // 1 hour ago
+        console.log(now)
         console.log(oneHourAgo)
         // Find existing events with the "location_random_fact" tag created within the last hour
         const events = await eventsCollection.find({
@@ -55,24 +55,24 @@ router.get("/", async (req, res) => {
         }
 
         // If we have valid events, return them
-        if (validEvents.length > 0) {
-            console.log("Returning existing valid events.");
-            return res.json({
-                events: validEvents.map(event => ({
-                    type: "Feature",
-                    geometry: event.geometry,
-                    properties: event.properties,
-                    id: event._id
-                }))
-            });
-        }
+        // if (validEvents.length > 0) {
+        //     console.log("Returning existing valid events.");
+        //     return res.json({
+        //         events: validEvents.map(event => ({
+        //             type: "Feature",
+        //             geometry: event.geometry,
+        //             properties: event.properties,
+        //             id: event._id
+        //         }))
+        //     });
+        // }
 
         console.log("No valid events found. Generating new one...");
 
         const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt = "my app generates facts about a location. generate a historic, geophique, scientifique,... fact about a random location in the world (anywhere, from big to little historique event, make sure it's unique at each request and from diffrent country) as geojson type Feature format with properties (fact, adress, location)";
+        const prompt = "my app generates facts about a location. generate a really funny and true historic, geophique, scientifique,... fact about a random location in the world (anywhere, from big to little historique event, make sure it's unique at each request and from diffrent country) as geojson type Feature format with properties (fact, adress, location)";
 
         const result = await model.generateContent(prompt);
         console.log(result.response.text());
