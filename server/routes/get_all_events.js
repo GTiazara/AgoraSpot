fa23bf2248b06459c4dce3685861d4755290ce9b
gtiazara <tiazaragaetan@gmail.com>
2025-03-06 23:10:25 +0100

    test live chat

--- server/routes/get_all_events.js
+++ server/routes/get_all_events.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
         const events = await db.collection("events")
             .find({}) // You can also use filters if needed
             .toArray();
-        console.log(events)
+        // console.log(events)
         // Respond with events as GeoJSON features
         const geoJSON = {
             type: "FeatureCollection",
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
             }))
         };
 
-        console.log("all data", geoJSON)
+        // console.log("all data", geoJSON)
 
         res.json({ events: geoJSON.features });
 
