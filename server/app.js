7fc649921596382b816f7a492fd3fdaabb2fa4d5
gtiazara <tiazaragaetan@gmail.com>
2025-03-08 12:38:43 +0100

    socket.io cors

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const http = require('http');
 const debug = require('debug')('myapp:server');
 
 
-
 // Import routes
 // const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
@@ -27,7 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({ 
+  // origin: "*", // Allow all origins
+  // methods: ["GET","OPTIONS","PATCH","DELETE","POST","PUT"],
+  // allowedHeaders: ["Content-Type"]
+}));
 
 // Define routes
 // app.use('/', getEventRouter);
@@ -69,6 +72,25 @@ const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+
+//
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }});
+io.on('connection', function(socket) {
+  console.log(socket.id)
+    socket.on('SEND_MESSAGE', function(data) {
+      console.log("message reveibelsfsfsdqfd")
+      console.log(data)
+        io.emit('MESSAGE', data)
+    });
+
+    socket.on('disconnect', () => {
+      console.log("Client disconnected:", socket.id);
+  });
+
+});
 
 // Helper functions
 function normalizePort(val) {
